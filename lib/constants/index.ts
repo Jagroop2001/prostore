94d1120c8a4a5189b10ad0149b864ffda43a4df0
Jagroop2001 <jagroopofficial2000@gmail.com>
2025-03-09 12:36:22 +0530

    Initial Commit of Prostore project nextjs

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME ='Prostore';
+export const APP_DESCRIPTION ='Modern E-commerce for everyone';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
